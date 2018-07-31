7f969e34d03af47ff3abb022bb98430d81aca400
Jason <kingmax_res@163.com>
2018-07-31 18:32:30 +0800

    + maya dotnet plugin::csHelloMaya

--- PycharmMaya/test2maya.py
+++ PycharmMaya/test2maya.py
@@ -6,4 +6,10 @@ print(sys.executable)
 
 from maya import cmds
 
-print(cmds.polyCube())
+#print(cmds.polyCube())
+
+from maya import OpenMaya
+
+selObjs = OpenMaya.MSelectionList()
+OpenMaya.MGlobal.getActiveSelectionList(selObjs)
+print(selObjs.length())
---
+++ csHelloMaya/csHelloMaya.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Autodesk.Maya.OpenMaya;
+
+[assembly:MPxCommandClass(typeof(MayaCSPlugin.csHelloMaya), "csHelloMaya")]
+[assembly:ExtensionPlugin(typeof(MayaCSPlugin.csHelloMayaPlugin), "kingmax_res@163.com | 184327932@qq.com | iJasonLee@WeChat")]
+
+namespace MayaCSPlugin
+{
+
+    public class csHelloMayaPlugin : IExtensionPlugin
+    {
+        public string GetMayaDotNetSdkBuildVersion()
+        {
+            return "2018.07.25.01";
+        }
+
+        public bool InitializePlugin()
+        {
+            return true;
+        }
+
+        public bool UninitializePlugin()
+        {
+            return true;
+        }
+    }
+
+    public class csHelloMaya : MPxCommand, IMPxCommand
+    {
+        public override void doIt(MArgList args)
+        {
+            //base.doIt(args);
+            MGlobal.displayInfo("Hello, Maya. This is from Maya CSharp Plugin! 你好！");
+        }
+    }
+}
